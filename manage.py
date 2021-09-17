cb285d886fe5026d0bc1738c0f8144461f70982b
adepeter <adepeter26@gmail.com>
2021-09-17 17:45:02 +0100

    Fixing uwsgi file

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'statesng.settings')
+    statesng_environment = os.environ.get('STATESNG_ENVIRONMENT', 'statesng.settings.development')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', statesng_environment)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- statesng/settings/production.py
+++ statesng/settings/production.py
@@ -3,4 +3,15 @@ import os
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = os.environ.get('STATES_NG_HOST')
+ALLOWED_HOSTS = os.environ.get('STATESNG_HOST')
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('STATESNG_DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.environ.get('STATESNG_DB_NAME', 'statesng_db'),
+        'USER': os.environ.get('STATESNG_DB_USER', 'statesng_user'),
+        'PASSWORD': os.environ.get('STATESNG_DB_PASSWORD', 'statesng_password'),
+        'HOST': os.environ.get('STATESNG_DB_HOST', 'localhost'),
+        'PORT': os.environ.get('STATESNG_DB_PORT', '5432')
+    }
+}
