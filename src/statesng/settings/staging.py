a6de808bf578d6e4c5598bf3ee34f19e1e61de94
adepeter <adepeter26@gmail.com>
2021-09-18 21:13:22 +0100

    Fixing permission issues for migrations

--- src/statesng/settings/staging.py
+++ src/statesng/settings/staging.py
@@ -4,13 +4,20 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'statesng_db',
+#         'USER': 'statesng_user',
+#         'PASSWORD': 'statesng_password',
+#         'HOST': 'postgres',
+#         'PORT': 5432
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'statesng_db',
-        'USER': 'statesng_user',
-        'PASSWORD': 'statesng_password',
-        'HOST': 'postgres',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
