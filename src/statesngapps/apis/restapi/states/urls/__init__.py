b6f582868562ed5e6dfa9f97140d1fa5f92bc74d
adepeter <adepeter26@gmail.com>
2021-09-20 19:07:38 +0100

    Setup for production

--- src/statesngapps/apis/restapi/states/urls/__init__.py
+++ src/statesngapps/apis/restapi/states/urls/__init__.py
@@ -1,8 +1,8 @@
-from django.urls import path, re_path, include
+from django.urls import path, include
 
 app_name = 'states'
 
 urlpatterns = [
-    path('states/', include('statesngapps.api.restapi.states.urls.state', namespace='state')),
-    path('lgas/', include('statesngapps.api.restapi.states.urls.lga', namespace='lga')),
+    path('states/', include('statesngapps.apis.restapi.states.urls.state', namespace='state')),
+    path('lgas/', include('statesngapps.apis.restapi.states.urls.lga', namespace='lga')),
 ]
