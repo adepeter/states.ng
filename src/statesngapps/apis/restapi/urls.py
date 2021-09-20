bcd62b275867ac243cc5f1d1662965ecc149d9cb
adepeter <adepeter26@gmail.com>
2021-09-20 19:00:26 +0100

    Setup for production

--- src/statesngapps/apis/restapi/urls.py
+++ src/statesngapps/apis/restapi/urls.py
@@ -5,13 +5,13 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 app_name = 'restapi'
 
 urlpatterns = [
-    path('', include('statesngapps.api.restapi.states.urls', namespace='states')),
+    path('', include('statesngapps.apis.restapi.states.urls', namespace='states')),
 ]
 
 urlpatterns += [
     path('docs/', include([
         path('', SpectacularAPIView.as_view(), name='schema'),
-        path('swagger/', SpectacularSwaggerView.as_view(url_name='statesng:restapi:schema'), name='swagger-ui'),
-        path('redoc/', SpectacularRedocView.as_view(url_name='statesng:restapi:schema'), name='redoc'),
+        path('swagger/', SpectacularSwaggerView.as_view(url_name='statesng:api:restapi:schema'), name='swagger-ui'),
+        path('redoc/', SpectacularRedocView.as_view(url_name='statesng:api:restapi:schema'), name='redoc'),
     ]))
 ]
--- src/statesngapps/urls.py
+++ src/statesngapps/urls.py
@@ -4,4 +4,5 @@ app_name = 'statesng'
 
 urlpatterns = [
     path('', include('statesngapps.states.urls', namespace='states')),
+    path('', include('statesngapps.apis.urls', namespace='api')),
 ]
