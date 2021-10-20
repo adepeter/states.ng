5d54852c5db169bf918a0a0e2d83507c4a65ac65
adepeter <adepeter26@gmail.com>
2021-10-20 16:35:49 +0100

    Changed some details in env file

--- src/statesng/settings/production.py
+++ src/statesng/settings/production.py
@@ -4,8 +4,10 @@ from .base import *
 
 SECRET_KEY = os.environ.get('STATESNG_SECRET_KEY')
 ALLOWED_HOST = os.environ.get('ROOT_DOMAIN_NAME')
+
+
 DEBUG = False
-ALLOWED_HOSTS = ['%s', 'www.%s', 'api.%s'.format(ALLOWED_HOST)]
+ALLOWED_HOSTS = ['{domain}', 'www.{domain}', 'api.{domain}'.format(domain=ALLOWED_HOST)]
 
 INSTALLED_APPS += [
     'django_hosts'
