64ef48098985e492fd0de3bfb1fa5c1d76e462aa
adepeter <adepeter26@gmail.com>
2021-10-27 23:38:22 +0100

    Bash Script Implemented for deploy

--- setup.sh
+++ setup.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+declare -A ARRAY_OF_ENV_FILES
+
 PROJECT_NAME='STATESNG API PROJECT'
 BACKEND_SERVICE_NAME='statesng'
 CONFIG_DIR='configs'
 ENV_FILES_DIR="${CONFIG_DIR}/envs/"
 SUBDOMAINS=("api" "cpanel" "pages" "www" "admin")
+ROOT_DOMAIN_NAME="statesng.com.ng"
+PGADMIN4_SUBDOMAIN_NAME="pgadmin4"
+ARRAY_OF_ENV_FILES=(
+  [nginxproxy]="nginxproxy.env"
+  [nginxproxy_letsencrypt]="nginxproxy_acme.env"
+  [pgadmin]="pgadmin4.env"
+  [postgres]="postgres.env"
+  [redis]="redis.env"
+  [statesng]="statesng.env"
+)
 
 close_color="\e[0m"
 color_close="${close_color}"
@@ -19,6 +31,7 @@ blink="\e[5m"
 bold="\e[1m"
 dim_color="\e[2m"
 hide_color="\e[8m"
+italic="\e[3m"
 underline="\e[4m"
 
 close_blink="\e[25m"
@@ -48,6 +61,23 @@ majenta_bg="\e[45m"
 red_bg="\e[41m"
 white_bg="\e[107m"
 yellow_bg="\e[43m"
+
+generate_env_files() {
+  local config_env_dir="${ENV_FILES_DIR%/}"
+  mkdir -p "${config_env_dir}"
+  for env_file in "${!ARRAY_OF_ENV_FILES[@]}"; do
+    local env_file_with_path
+    env_file_with_path="${config_env_dir%/}/${ARRAY_OF_ENV_FILES[${env_file}]}"
+    if [[ -f "${env_file_with_path}" ]]; then
+      rm -r "${env_file_with_path}"
+    fi
+    touch "${env_file_with_path}"
+  done
+}
+
+create_env_files() {
+  generate_env_files
+}
 
 base_docker_command_for_backend() {
   echo "docker-compose exec ${BACKEND_SERVICE_NAME} python manage.py"
@@ -128,7 +158,9 @@ setup_allowed_hosts() {
 }
 
 setup_postgres() {
-  local postgres_db_name postgres_db_user postgres_db_password postgres_env_file="${ENV_FILES_DIR%/}/postgress.env"
+  local postgres_db_name postgres_db_user postgres_db_password
+  local postgres_env_file="${ENV_FILES_DIR}/${ARRAY_OF_ENV_FILES[postgres]}"
+  local statesng_env_file="${ENV_FILES_DIR}/${ARRAY_OF_ENV_FILES[statesng]}"
   echo "Setting up postgres"
   read -p "Enter your postgres DB name: " postgres_db_name
   read -p "Enter your postgres DB username: " postgres_db_user
@@ -138,11 +170,33 @@ setup_postgres() {
 
   echo -e \
     "POSTGRES_DB=${postgres_db_name}\nPOSTGRES_USER=${postgres_db_user}\nPOSTGRES_PASSWORD=${postgres_db_password}" \
-    >${postgres_env_file}
+    >"${postgres_env_file}"
+  echo -e \
+    "STATESNG_DB_NAME=${postgres_db_name}\nSTATESNG_DB_USER=${postgres_db_user}\nSTATESNG_DB_PASSWORD=${postgres_db_password}" \
+    >>"${statesng_env_file}"
 }
 
 setup_pgadmin4() {
   echo "Setting up pgadmin4"
+
+  local pgadmin4_email_address pgadmin4_password is_valid=false
+  local pgadmin4_env_file="${ENV_FILES_DIR}/${ARRAY_OF_ENV_FILES[pgadmin]}"
+    until $is_valid; do
+      read -p "Enter PGADMIN4 login e-mail address: " pgadmin4_email_address
+      validate_email "${pgadmin4_email_address}"
+      if [[ $? -eq 0 ]]; then
+        echo "${pgadmin4_email_address}"
+        is_valid=true
+      fi
+    done
+  until [[ -n ${pgadmin4_password} ]]; do
+    echo "Please enter your PGADMIN4 login password: "
+    read -rs
+    pgadmin4_password=${REPLY}
+  done
+  echo -e "PGADMIN_DEFAULT_EMAIL=${pgadmin4_email_address}" >>"${pgadmin4_env_file}"
+  echo -e "PGADMIN_DEFAULT_PASSWORD=${pgadmin4_password}" >>"${pgadmin4_env_file}"
+
 }
 
 make_migrations() {
@@ -164,8 +218,7 @@ post_installation_setup() {
 }
 
 setup_domain_name() {
-  local clean_domain domain_name is_valid=false
-  echo -e "DOMAIN NAME SETUP"
+  local clean_domain is_valid=false
   until $is_valid; do
     read -p "Please enter your domain name: " domain_name
     validate_domain_name "${domain_name}"
@@ -190,40 +243,55 @@ setup_email() {
 }
 
 setup_statesng() {
+  local secret_key domain_name subdomains
+  local statesng_env_file="${ENV_FILES_DIR}/${ARRAY_OF_ENV_FILES[statesng]}"
   echo "Backend configuration setup"
-}
-
-global_configuration_setup() {
-  local domain_name email_address secret_key subdomains
   secret_key=$(generate_secret_code)
-  echo -e "GENERAL CONFIGURATION"
-  echo "This is a one time configuration setup that will be shared across all other subsequent installation"
   domain_name=$(setup_domain_name)
-  email_address=$(setup_email)
-  subdomains="${domain_name}"
   for subdomain in ${SUBDOMAINS[*]}; do
-    if [[ ${SUBDOMAINS[*]: -1} != "${subdomain}" ]]; then
+    if [[ ${SUBDOMAINS[0]} = "${subdomain}" ]]; then
+      subdomains="${domain_name},"
+    elif [[ ${SUBDOMAINS[*]: -1} != "${subdomain}" ]]; then
       subdomains+="${subdomain}.${domain_name},"
-    elif [[ ${SUBDOMAINS[0]} = "${subdomain}" ]]; then
-      subdomains+=","
     else
       subdomains+="${subdomain}.${domain_name}"
     fi
   done
-  echo "${subdomains}"
-  #    echo -e "ROOT_DOMAIN_NAME=${subdomains}\nLETSENCRYPT_EMAIL=${email_address}\nSTATESNG_SECRET_KEY=${secret_key}" > \
-  #    "${ENV_FILES_DIR}/statesng.env"
+  echo -e "ROOT_DOMAIN_NAME=${domain_name}\nSTATESNG_SECRET_KEY=${secret_key}" >>"$statesng_env_file"
+  echo -e "VIRTUAL_HOST=${subdomains}\nLETSENCRYPT_HOST=${subdomains}" >>"$statesng_env_file"
+
+  local pgadmin4_env_file="${ENV_FILES_DIR}/${ARRAY_OF_ENV_FILES[pgadmin]}"
+  local pgadmin4_domain="${PGADMIN4_SUBDOMAIN_NAME}.${domain_name}"
+  echo -e "VIRTUAL_HOST=${pgadmin4_domain}\nLETSENCRYPT_HOST=${pgadmin4_domain}" >>"${pgadmin4_env_file}"
+
 }
 
-global_configuration_setup
+global_configuration_setup() {
+  local email_address
+  local nginxproxy_acme_env_file="${ENV_FILES_DIR}/${ARRAY_OF_ENV_FILES[nginxproxy_letsencrypt]}"
+  local statesng_env_file="${ENV_FILES_DIR}/${ARRAY_OF_ENV_FILES[statesng]}"
+  local pgadmin4_env_file="${ENV_FILES_DIR}/${ARRAY_OF_ENV_FILES[pgadmin]}"
+
+  echo -e "GENERAL CONFIGURATION"
+  echo "This is a one time configuration setup that will be shared across all other subsequent installation"
+  email_address=$(setup_email)
+  echo "DEFAULT_EMAIL=${email_address}" >"$nginxproxy_acme_env_file"
+  echo -e "LETSENCRYPT_EMAIL=${email_address}" >>"$statesng_env_file"
+  echo -e "LETSENCRYPT_EMAIL=${email_address}" >>"$pgadmin4_env_file"
+
+}
 
 start() {
-  echo -e "${bold}WELCOME TO ${PROJECT_NAME}${close_bold}"
+  echo -e "${bold}WELCOME TO ${PROJECT_NAME}${close_color}"
   echo ""
   echo ""
+  create_env_files
   global_configuration_setup
   setup_postgres
   setup_statesng
   setup_pgadmin4
+  docker-compose up -d
   post_installation_setup
 }
+
+start
