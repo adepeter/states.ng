bd8b1c95e0461154d288c0a769dc271fc942212a
adepeter <adepeter26@gmail.com>
2021-10-28 16:09:27 +0100

    Slight fix

--- src/statesng/settings/production.py
+++ src/statesng/settings/production.py
@@ -21,14 +21,28 @@ DATABASES = {
         'USER': os.environ.get('STATESNG_DB_USER', 'statesng_user'),
         'PASSWORD': os.environ.get('STATESNG_DB_PASSWORD', 'statesng_password'),
         'HOST': os.environ.get('STATESNG_DB_HOST', 'localhost'),
-        'PORT': os.environ.get('STATESNG_DB_PORT', '5432')
+        'PORT': os.environ.get('STATESNG_DB_PORT', 5432)
     }
 }
 
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-#
-# SECURE_SSL_REDIRECT = True
-#
-# USE_X_FORWARDED_HOST = True
-#
-# USE_X_FORWARDED_PORT = True
+
+# Caching
+# https://docs.djangoproject.com/en/3.2/topics/cache/#setting-up-the-cache
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECURE_SSL_REDIRECT = True
+
+USE_X_FORWARDED_HOST = True
+
+USE_X_FORWARDED_PORT = True
