2cb4b8e63f594d5d0f037a398dba01aa9ce54c74
adepeter <adepeter26@gmail.com>
2021-10-28 12:29:28 +0100

    Add ROOT_DOMAIN_NAME env

--- src/statesng/settings/production.py
+++ src/statesng/settings/production.py
@@ -5,13 +5,13 @@ ALLOWED_HOST = os.environ.get('ROOT_DOMAIN_NAME', 'statesng.com.ng')
 
 
 DEBUG = False
-# ALLOWED_HOSTS = [
-#     '{domain}',
-#     'www.{domain}',
-#     'cpanel.{domain}',
-#     'pages.{domain}',
-#     'api.{domain}'.format(domain=ALLOWED_HOST)
-# ]
+ALLOWED_HOSTS = [
+    '{domain}',
+    'www.{domain}',
+    'cpanel.{domain}',
+    'pages.{domain}',
+    'api.{domain}'.format(domain=ALLOWED_HOST)
+]
 
 INSTALLED_APPS += [
     'django_hosts',
